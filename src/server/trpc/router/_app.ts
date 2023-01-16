0edf98e6907fb49dcb444f9d32d70854496de8d2
prenaissance <alexandru.andries@isa.utm.md>
2023-01-16 16:04:00 +0200

    fix(api): removed unused trcp example router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -2,11 +2,9 @@ import { router } from "../trpc";
 import { albumsRouter } from "./albums";
 import { artistsRouter } from "./artists";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { songsRouter } from "./songs";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   songs: songsRouter,
   artists: artistsRouter,
