27a3a9efbb2385cad523caf85a59a23d936fea56
prenaissance <alexandru.andries@isa.utm.md>
2023-02-01 23:43:05 +0200

    feat: Added playlist page placeholders with SSG

---
+++ src/pages/playlists/[title].tsx
@@ -0,0 +1,56 @@
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import superjson from "superjson";
+
+import { getServerAuthSession } from "@server/common/get-server-auth-session";
+import { createContextInner } from "@server/trpc/context";
+import { appRouter } from "@server/trpc/router/_app";
+import { trpc } from "@utils/trpc";
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext<{
+    title: string;
+  }>,
+) => {
+  const { title } = context.params!;
+  const session = await getServerAuthSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `api/auth/signin?callbackUrl=${encodeURIComponent(
+          context.resolvedUrl,
+        )}`,
+        permanent: false,
+      },
+    };
+  }
+
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: await createContextInner({ session }),
+    transformer: superjson,
+  });
+
+  await ssg.playlists.getPlaylist.prefetch({ title });
+
+  return {
+    props: {
+      dehydratedState: ssg.dehydrate(),
+      title,
+    },
+  };
+};
+
+function PlaylistPage({
+  title,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const playlistQuery = trpc.playlists.getPlaylist.useQuery({ title });
+  return (
+    <>
+      <div>PlaylistPage</div>
+    </>
+  );
+}
+
+export default PlaylistPage;
